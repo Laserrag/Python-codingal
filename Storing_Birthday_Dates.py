18f85b38e693cf4844c90437907ba18e973384dc
Laserrag <hellokishp@gmail.com>
2026-06-26 17:22:32 +0530

    Storing birthday dates

---
+++ Storing_Birthday_Dates.py
@@ -0,0 +1,13 @@
+#Names of Raj's friends- Aarav, Maya, Vivaan, Ananya, Kabir.
+#The dates are arranged in DD/MM/YY.
+aarav_date = "18/4/13"
+maya_date = "13/2/14"
+vivaan_date = "5/6/13"
+ananya_date = "3/8/15"
+kabir_date = "28/9/14"
+
+print("The age of Aarav is", aarav_date)
+print("The age of Maya is", maya_date)
+print("The age of Vivaan is",vivaan_date)
+print("The age of Ananya is", ananya_date)
+print("The age of Kabir is", kabir_date)
