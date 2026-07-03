38aca8829a87e0e197cbf81989a4c8af6abf21f8
Laserrag <hellokishp@gmail.com>
2026-07-03 19:26:52 +0530

    My congrtulations message

---
+++ Congraulations_message.py
@@ -0,0 +1,7 @@
+#Project:Congratulations message    
+
+message = str(input("Please enter your message: "))
+#Ram now types "congartulations" but in small case
+
+print(message.upper())
+#Now the message will come as "CONGRATULATIONS"
