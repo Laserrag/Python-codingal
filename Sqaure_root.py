5dbb01c98c9c289d92d62f813dacc39e6e3ab97e
Laserrag <hellokishp@gmail.com>
2026-07-05 15:51:21 +0530

    Square root

---
+++ Sqaure_root.py
@@ -0,0 +1,5 @@
+#The user can give decimal inputs.
+square = float(input("Please enter a number: "))
+result = square ** 0.5
+#This will divided the number by 2.We could also write it as "result = square / 2"
+print("The square root is:", result)
